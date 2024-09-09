388d767cd761932b77740055449086f07281ebb4
David Chan <davidchan@Davids-Macbook-Air-2.local>
2024-09-09 23:06:19 +0800

    added joke

--- bot/bot.py
+++ bot/bot.py
@@ -5,7 +5,7 @@ bot = telebot.TeleBot(os.getenv('BOT_TOKEN'))
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "Hello! Your bot is working!")
+    bot.reply_to(message, "Hello! Your bot is working! Also Aayush smells!")
 
 if __name__ == '__main__':
     bot.polling(none_stop=True)
