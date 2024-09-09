5ea8e587818900404399ca05087577a3a0634af3
David Chan <davidchan@Davids-Macbook-Air-2.local>
2024-09-09 21:41:09 +0800

    remove flask wrapper

--- bot/bot.py
+++ bot/bot.py
@@ -1,22 +1,11 @@
-from flask import Flask
 import telebot
 import os
-import logging
 
-logging.basicConfig(level=logging.DEBUG)
-
-app = Flask(__name__)
 bot = telebot.TeleBot(os.getenv('BOT_TOKEN'))
 
-@app.route('/')
-def home():
-    return "Bot is running!"
-
-# Add bot handler for /start command
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     bot.reply_to(message, "Hello! Your bot is working!")
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, threaded=True)
     bot.polling(none_stop=True)
