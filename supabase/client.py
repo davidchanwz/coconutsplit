6abc81a4b56906145efc48f83a5e160a24435ab6
David Chan <davidchan@Davids-Macbook-Air-2.local>
2024-09-10 16:42:57 +0800

    editted

--- supabase/client.py
+++ supabase/client.py
@@ -4,4 +4,7 @@ import os
 SUPABASE_URL: str = os.getenv('SUPABASE_URL')
 SUPABASE_KEY: str =  os.getenv('SUPABASE_KEY')
 
+if not SUPABASE_URL or not SUPABASE_KEY:
+    raise ValueError("Supabase URL or Key is not set in the environment variables")
+
 supa: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
