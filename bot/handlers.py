a5772852f1f89b45f1f6cdc6ed67b21204544c3d
David Chan <davidchan@Davids-Macbook-Air-2.local>
2024-09-13 03:16:23 +0800

    rerun

--- bot/handlers.py
+++ bot/handlers.py
@@ -4,6 +4,8 @@ from telebot import types
 from bot.classes import Group, User, Expense, Settlement
 import uuid
 
+# comment
+
 group_data = {}  # To temporarily store active group data during creation
 
 def register_handlers(bot):
