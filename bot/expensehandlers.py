f482b5c60b9a1f07b5a95a994b7d8199523a3b5a
Ben926 <benjamin.koh.ts@gmail.com>
2025-03-12 02:35:27 +0800

    Fix error for settlements2

--- bot/expensehandlers.py
+++ bot/expensehandlers.py
@@ -498,8 +498,8 @@ def register_expense_handlers(bot):
             # Format the settlements
             formatted_output = []
             for settlement in settlements:
-                from_user = settlement.from_user
-                to_user = settlement.to_user
+                from_user = settlement.from_user.replace('_', '\\_')
+                to_user = settlement.to_user.replace('_', '\\_')
                 formatted_output.append(f"{from_user.username} paid {to_user.username} ${settlement.amount:.2f} on {settlement.created_at.strftime('%Y-%m-%d')}")
 
             # Send the formatted list of settlements
