bce413695bef722eebe63d0d2526837112c25b8e
Ben926 <benjamin.koh.ts@gmail.com>
2025-04-05 22:37:56 +0800

    Fix floating precision 2

--- bot/utils.py
+++ bot/utils.py
@@ -27,11 +27,11 @@ def simplify_debts(balances):
             debtor_id, debt_amount = debtors.pop()
 
             # Calculate the minimum of what the debtor owes and what the creditor is owed
-            if amount >= 0.01: 
-                amount = min(credit_amount, debt_amount)
+            amount = min(credit_amount, debt_amount)
 
             # Record this transaction
-            simplified_debts.append((debtor_id, creditor_id, amount))
+            if amount >= 0.01:           
+                simplified_debts.append((debtor_id, creditor_id, amount))
 
             # Adjust the remaining balances
             credit_amount -= amount
